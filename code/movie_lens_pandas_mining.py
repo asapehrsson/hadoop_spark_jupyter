b5c1bfdb330850f7a868978c883e234f2e3cfefb
asapehrsson <asa.pehrsson@gmail.com>
2018-03-28 17:32:25 +0200

    Exploring movielens dataset con't

--- code/movie_lens_pandas_mining.py
+++ code/movie_lens_pandas_mining.py
@@ -4,20 +4,21 @@ import movielens_helper as mh
 
 pd.options.display.max_rows = 10
 
-ROOT_PATH = '/Users/asapehrsson/dev/learn/hadoop_spark_jupyter/'
-
-data = pd.read_csv(ROOT_PATH + '/data/ml-latest-small/ratings.csv')
+data = mh.get_ratings_df(mh.ROOT_PATH)
+print(data)
 data = mh.add_year_week(data)
-
+print(data)
 ratings = data \
     .groupby(mh.YEAR_WEEK_COL_NAME) \
     .count() \
     .sort_values(ascending=False, by="rating")
 
 ratings.reset_index(inplace=True, drop=False)
+print(ratings)
 
 
-all_year_weeks = mh.prepare_all_year_weeks(data)
+all_year_weeks = mh.get_year_weeks_datetime_df(mh.ROOT_PATH, data)
+print(all_year_weeks)
 
 foo = mh.fill_in_the_blanks(ratings, all_year_weeks)
 # foo.head()
--- code/movielens_helper.py
+++ code/movielens_helper.py
@@ -1,8 +1,16 @@
 from __future__ import print_function
 
-from datetime import datetime, timedelta
+import os
+from datetime import timedelta
 
 import pandas as pd
+
+_PD_DATETIME_FACTOR = 1000000000
+
+ROOT_PATH = '/Users/asapehrsson/dev/learn/hadoop_spark_jupyter/'
+
+Y_W_TIMESTAMP_LT_COL_NAME = 'lt_timestamp'
+Y_W_TIMESTAMP_GE_COL_NAME = 'ge_timestamp'
 
 RATINGS_COL_NAME = 'ratings'
 DATETIME_COL_NAME = 'datetime'
@@ -11,71 +19,155 @@ YEAR_WEEK_COL_NAME = 'y_w'
 
 YEAR_WEEK_FORMAT_STR = "%d%02d"
 
-
-def add_year_week(data):
-    data[DATETIME_COL_NAME] = data[TIMESTAMP_COL_NAME].apply(lambda x: datetime.fromtimestamp(x))
-    data[YEAR_WEEK_COL_NAME] = data[DATETIME_COL_NAME].apply(lambda x: YEAR_WEEK_FORMAT_STR % (x.year, x.week))
-    return data
+MOVIE_LENS_DATA_RELATIVE_PATH = 'data/ml-latest-small'
 
 
-def fill_in_the_blanks(data, all_weeks):
-    start_index = all_weeks[all_weeks[YEAR_WEEK_COL_NAME] == data[YEAR_WEEK_COL_NAME].min()].index.item()
-    stop_index = all_weeks[all_weeks[YEAR_WEEK_COL_NAME] == data[YEAR_WEEK_COL_NAME].max()].index.item()
+def year_week_from_date_time(datetime):
 
-    all_weeks_for_movie = all_weeks[start_index: stop_index]
-
-    # https://data-lessons.github.io/library-python/04-merging-data/
-    # this is a left outer join
-    result = pd.merge(all_weeks_for_movie, data, how='left', left_on=YEAR_WEEK_COL_NAME, right_on=YEAR_WEEK_COL_NAME)
+    if datetime.month == 1 and datetime.week > 50:
+        result = (datetime.year - 1) * 100 + datetime.week
+    else:
+        result = datetime.year * 100 + datetime.week
 
     return result
 
 
-def prepare_all_year_weeks(data):
+def year_week_from_timestamp(timestamp):
+    datetime = pd.to_datetime(timestamp * _PD_DATETIME_FACTOR)
+    return year_week_from_date_time(datetime)
+
+
+def add_year_week(df):
+    # df[DATETIME_COL_NAME] = df[TIMESTAMP_COL_NAME].apply(lambda x: datetime.fromtimestamp(x))
+    # df[YEAR_WEEK_COL_NAME] = df[DATETIME_COL_NAME].apply(lambda x: x.year * 100 + x.week)
+
+    df[YEAR_WEEK_COL_NAME] = df[TIMESTAMP_COL_NAME].apply(lambda x: year_week_from_timestamp(x))
+
+    print(df.dtypes)
+    print(df[YEAR_WEEK_COL_NAME].max())
+    return df
+
+
+def fill_in_the_blanks(data_df, all_weeks_df):
+    start_index = all_weeks_df[all_weeks_df[YEAR_WEEK_COL_NAME] == data_df[YEAR_WEEK_COL_NAME].min()].index.item()
+    stop_index = all_weeks_df[all_weeks_df[YEAR_WEEK_COL_NAME] == data_df[YEAR_WEEK_COL_NAME].max()].index.item()
+
+    all_weeks_for_movie = all_weeks_df[start_index: stop_index + 1]
+
+    # https://data-lessons.github.io/library-python/04-merging-data/
+    # this is a left outer join
+    result = pd.merge(all_weeks_for_movie, data_df, how='left', left_on=YEAR_WEEK_COL_NAME, right_on=YEAR_WEEK_COL_NAME)
+
+    return result
+
+
+def _prepare_all_year_weeks_df(date_time_min, date_time_max):
     one_day = timedelta(hours=24)
     one_week = timedelta(days=7)
 
-    current = data[DATETIME_COL_NAME].min() - one_week
+    current = date_time_min - (one_week * 2)
 
     if current.dayofweek > 0:
         current = current - timedelta(days=current.dayofweek)
 
-    latest = data[DATETIME_COL_NAME].max() + one_week
+    last_date_in_series = date_time_max + timedelta(weeks=2)
 
     # was facing a strange bug here - got 201852, then 201801 and then 201901 when using
     # subtracting 'current' from last saved timestamp and comparing with 'one_week'
 
-    year = 0
-    week = 0
+    week = current.week
+    year = current.year
+    pending = current
 
     weeks = []
 
-    while current <= latest:
-        if current.week > week and current.year == year or current.year > year:
-            weeks.append(YEAR_WEEK_FORMAT_STR % (current.year, current.week))
+    while current <= last_date_in_series:
+        if (current.week > week and current.year == year) or (current.year > year and current.week == 1):
+            row = [year_week_from_date_time(pending), "%d" % pending.timestamp(), "%d" % current.timestamp()]
+            weeks.append(row)
+            pending = current
             week = current.week
             year = current.year
 
         current = current + one_day
 
-    all_year_weeks = pd.DataFrame({YEAR_WEEK_COL_NAME: weeks})
-    all_year_weeks[RATINGS_COL_NAME] = 0
+    all_year_weeks = pd.DataFrame(weeks)
+    all_year_weeks.columns = [YEAR_WEEK_COL_NAME, Y_W_TIMESTAMP_GE_COL_NAME, Y_W_TIMESTAMP_LT_COL_NAME]
+
     return all_year_weeks
 
 
-def main():
-    # test with a small data set that passes year end
-    data = pd.DataFrame({TIMESTAMP_COL_NAME: [1545609600, 1546300800]})
-    print(data)
+def get_year_weeks_datetime_df(root_path, data_df=None):
+    path = os.path.join(root_path, MOVIE_LENS_DATA_RELATIVE_PATH)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    data = add_year_week(data)
-    print(data)
+    path = os.path.join(path, "year_week.csv")
+    if os.path.isfile(path):
+        print('exist')
+        data = pd.read_csv(path)
+    else:
+        if data_df is None:
+            data_df = get_ratings_df(root_path)
 
-    all_year_weeks = prepare_all_year_weeks(data)
+        date_time_min = pd.to_datetime(data_df[TIMESTAMP_COL_NAME].min() * _PD_DATETIME_FACTOR)
+        date_time_max = pd.to_datetime(data_df[TIMESTAMP_COL_NAME].max() * _PD_DATETIME_FACTOR)
+
+        data = _prepare_all_year_weeks_df(date_time_min, date_time_max)
+
+        data.to_csv(path, index=False)
+
+    return data
+
+
+def get_ratings_df(root_path):
+    path = get_ratings_path(root_path)
+
+    if os.path.exists(path):
+        data = pd.read_csv(path)
+    else:
+        data = pd.DataFrame([[0, 0, 0, 0]])
+        print("no such file " + path)
+
+    data.columns = ['userId', 'movieId', 'rating', 'timestamp']
+    return data
+
+
+def get_ratings_path(root_path):
+    path = os.path.join(root_path, MOVIE_LENS_DATA_RELATIVE_PATH)
+    path = os.path.join(path, "ratings.csv")
+    return path
+
+
+def _test():
+    # a small data set, close to each side of year end
+    data_set_one = {TIMESTAMP_COL_NAME: [1545609600, 1546300800], 'color': ['red', 'green']}
+
+    # a small data set, close to beginning of two consecutive years
+    data_set_two = {TIMESTAMP_COL_NAME: [1522244593, 1546300800], 'color': ['red', 'green']}
+
+    data_df = pd.DataFrame(data_set_two)
+    print(data_df)
+
+    data_df = add_year_week(data_df)
+    print(data_df)
+
+    all_year_weeks = _prepare_all_year_weeks_df(data_df[DATETIME_COL_NAME].min(), data_df[DATETIME_COL_NAME].max())
     print(all_year_weeks)
 
-    data = fill_in_the_blanks(data, all_year_weeks)
-    print(data)
+    data_df = fill_in_the_blanks(data_df, all_year_weeks)
+    print(data_df)
+
+
+def main():
+    # data = get_year_weeks_datetime_df('/Users/asapehrsson/dev/learn/hadoop_spark_jupyter/')
+
+    # print(data)
+    # _test()
+
+    all_year_weeks = _prepare_all_year_weeks_df(pd.to_datetime(1102938409 * _PD_DATETIME_FACTOR),
+                                                pd.to_datetime(1168861609 * _PD_DATETIME_FACTOR))
+    print(all_year_weeks)
 
 
 if __name__ == '__main__':
